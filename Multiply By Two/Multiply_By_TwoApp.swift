cd35112342e09cb9be78c657052b2096abc4b95b
Rishi <rishi_j@icloud.com>
2025-06-30 14:25:55 +0100

    Initial Commit

---
+++ Multiply By Two/Multiply_By_TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiply_By_TwoApp.swift
+//  Multiply By Two
+//
+//  Created by Rishi Jansari on 30/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Multiply_By_TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
